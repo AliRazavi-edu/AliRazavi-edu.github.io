8a0a687a90cee79960475849e1ef4cd6a20a286b
alirazavi <seyed.ali.razavi.ebrahimi@gmail.com>
2020-12-31 02:55:36 +0330

    app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      {/*
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,6 +19,8 @@ function App() {
           Learn React
         </a>
       </header>
+      */}
+      <h1>Ali Razavi!</h1>
     </div>
   );
 }
