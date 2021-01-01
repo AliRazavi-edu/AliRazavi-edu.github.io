91a1a794c66f4ce2929e67cd29750ce6fbbb85fc
alirazavi <seyed.ali.razavi.ebrahimi@gmail.com>
2021-01-01 07:28:51 +0330

    code changes

--- src/App.js
+++ src/App.js
@@ -2,27 +2,31 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      {/*
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      */}
-      <h1>Ali Razavi!</h1>
-    </div>
-  );
+    return ( <
+        div className = "App" > {
+            /*
+                  <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                      Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <a
+                      className="App-link"
+                      href="https://reactjs.org"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Learn React
+                    </a>
+                  </header>
+                  */
+        } <
+        h1 > Ali Razavi! < /h1>
+
+        <
+        h2 > سیدعلی رضوی ابراهیمی < /h2>
+        div >
+    );
 }
 
 export default App;
